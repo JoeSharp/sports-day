62599dcb6b346f68ee18469e65b6880b19cad583
Joe Sharp <ratracejoe@hotmail.com>
2024-12-31 17:56:28 +0000

    keycloak realm harvesting sorted

---
+++ sports-day-service/local/auth/harvest_auth.sh
@@ -0,0 +1,3 @@
+docker exec sports-day-auth /opt/keycloak/bin/kc.sh export --dir /tmp
+docker cp sports-day-auth:/tmp/ratracejoe-realm.json .
+docker cp sports-day-auth:/tmp/ratracejoe-users-0.json .
--- sports-day-service/testing/login.sh
+++ sports-day-service/testing/login.sh
@@ -2,7 +2,7 @@ AUTH_HOST=http://localhost:8085
 CLIENT_ID="timesheets-service"
 CLIENT_SECRET="rX0uyWb89PxdeclkQoLMtmRtCLRxFlKy"
 USERNAME="joesharp"
-PASSWORD="quaverrat"
+PASSWORD="password"
 
 curl -X POST \
  $AUTH_HOST/realms/ratracejoe/protocol/openid-connect/token \
@@ -10,10 +10,10 @@ curl -X POST \
  -d "client_id=${CLIENT_ID}" \
  -d "client_secret=${CLIENT_SECRET}" \
  -d "username=${USERNAME}" \
- -d "password=${PASSWORD}'" \
+ -d "password=${PASSWORD}" \
  -d "grant_type=password" \
  -o token.json
 
 ACCESS_TOKEN=$(jq -r '.access_token' token.json)
-rm token.json
+#rm token.json
 echo "${ACCESS_TOKEN}"
