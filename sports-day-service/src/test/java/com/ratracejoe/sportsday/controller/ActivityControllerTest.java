9a5e34fb4dfde48e6ed925b7ba97d06c0c42510d
Joe Sharp <ratracejoe@hotmail.com>
2025-01-01 11:54:32 +0000

    keycloak working as test container

--- sports-day-service/src/test/java/com/ratracejoe/sportsday/controller/ActivityControllerTest.java
+++ sports-day-service/src/test/java/com/ratracejoe/sportsday/controller/ActivityControllerTest.java
@@ -1,13 +1,16 @@
 package com.ratracejoe.sportsday.controller;
 
 import com.ratracejoe.sportsday.model.ActivityDTO;
+import com.ratracejoe.sportsday.util.KeycloakExtension;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 
 import java.util.List;
 
@@ -18,15 +21,25 @@ class ActivityControllerTest {
     @Autowired
     private TestRestTemplate restTemplate;
 
+    @RegisterExtension
+    private static KeycloakExtension keycloakExtension = new KeycloakExtension();
+
+    @DynamicPropertySource
+    static void registerDynamicProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.security.oauth2.resourceserver.jwt.issuer-uri",
+                keycloakExtension::getIssuerUri);
+    }
+
     @Test
     void getActivities() {
+        HttpEntity<Void> request = new HttpEntity<>(keycloakExtension.getAuthHeaders());
         ResponseEntity<List<ActivityDTO>> response =
                 restTemplate.exchange("/activities", HttpMethod.GET,
-                    null,
+                    request,
                     new ParameterizedTypeReference<>() {
                     });
 
-        assertThat(response.getStatusCode().is2xxSuccessful()).isTrue();
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
         var activities = response.getBody();
 
         assertThat(activities).anyMatch(a -> "Running".equals(a.name()));
---
+++ sports-day-service/src/test/java/com/ratracejoe/sportsday/util/KeycloakExtension.java
@@ -0,0 +1,71 @@
+package com.ratracejoe.sportsday.util;
+
+
+import com.fasterxml.jackson.databind.JsonNode;
+import org.junit.jupiter.api.extension.AfterAllCallback;
+import org.junit.jupiter.api.extension.BeforeAllCallback;
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestClient;
+import org.testcontainers.containers.BindMode;
+import org.testcontainers.containers.GenericContainer;
+
+import java.util.Map;
+
+public class KeycloakExtension implements BeforeAllCallback, AfterAllCallback {
+    private static final String CLIENT_ID="timesheets-service";
+    private static final String CLIENT_SECRET="rX0uyWb89PxdeclkQoLMtmRtCLRxFlKy";
+    private static final String USERNAME="joesharp";
+    private static final String PASSWORD="password";
+
+    public static int KEYCLOAK_PORT = 8080;
+    private static final GenericContainer<?> keycloak =
+            new GenericContainer<>("quay.io/keycloak/keycloak:22.0")
+                .withCommand("start-dev --import-realm")
+                .withExposedPorts(KEYCLOAK_PORT)
+                .withClasspathResourceMapping("keycloak/", "/opt/keycloak/data/import/", BindMode.READ_ONLY);
+    private static RestClient keycloakClient;
+
+    public HttpHeaders getAuthHeaders() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setBearerAuth(login());
+        return headers;
+    }
+
+    public String login() {
+        MultiValueMap<String, String> map = new LinkedMultiValueMap<>();
+        map.add("client_id", CLIENT_ID);
+        map.add("client_secret", CLIENT_SECRET);
+        map.add("username", USERNAME);
+        map.add("password", PASSWORD);
+        map.add("grant_type", "password");
+
+        JsonNode response = keycloakClient.post()
+                .uri("/protocol/openid-connect/token")
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                .body(map)
+                .retrieve()
+                .body(JsonNode.class);
+        return response.get("access_token").asText();
+    }
+
+    @Override
+    public void beforeAll(ExtensionContext context) throws Exception {
+        keycloak.start();
+        keycloakClient = RestClient.builder()
+                .baseUrl(getIssuerUri())
+                .build();
+    }
+    @Override
+    public void afterAll(ExtensionContext context) throws Exception {
+        keycloak.stop();
+        keycloak.close();
+    }
+
+    public String getIssuerUri() {
+        return String.format("http://localhost:%d/realms/ratracejoe", keycloak.getMappedPort(KEYCLOAK_PORT));
+    }
+}
