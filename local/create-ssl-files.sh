0fe3599ec42b38489d211615ce91558b800654a7
Joe Sharp <ratracejoe@hotmail.com>
2025-03-07 16:13:49 +0000

    dont need the overall ssl files script

--- local/create-ssl-files.sh
+++
@@ -1,6 +0,0 @@
-./create-ca.sh ${APPLICATION_NAME}
-./create-server.sh ${APPLICATION_NAME} ${APPLICATION_NAME}-service
-./create-server.sh ${APPLICATION_NAME} ${APPLICATION_NAME}-ui
-./create-server.sh ${APPLICATION_NAME} ${APPLICATION_NAME}-client
-./create-server.sh ${APPLICATION_NAME} ${APPLICATION_NAME}-auth
-
