5aa5c02f7a44f99ded5017916d691ff273ef445f
Joe Sharp <ratracejoe@hotmail.com>
2025-09-28 17:02:13 +0100

    testing team controller membership

--- sports-day-service/spring-app/src/test/java/com/ratracejoe/sportsday/web/services/TeamControllerIntegrationTest.java
+++ sports-day-service/spring-app/src/test/java/com/ratracejoe/sportsday/web/services/TeamControllerIntegrationTest.java
@@ -2,13 +2,17 @@ package com.ratracejoe.sportsday.web.services;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import com.ratracejoe.sportsday.domain.model.CompetitorType;
 import com.ratracejoe.sportsday.rest.auth.model.LoginRequestDTO;
+import com.ratracejoe.sportsday.rest.model.CompetitorDTO;
+import com.ratracejoe.sportsday.rest.model.NewCompetitorDTO;
 import com.ratracejoe.sportsday.rest.model.NewTeamDTO;
 import com.ratracejoe.sportsday.rest.model.TeamDTO;
 import com.ratracejoe.sportsday.web.util.SportsDayContainers;
 import com.ratracejoe.sportsday.web.util.TestClient;
 import com.ratracejoe.sportsday.web.util.TestUser;
 import io.cucumber.java.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -45,12 +49,16 @@ public class TeamControllerIntegrationTest {
     SportsDayContainers.registerDynamicProperties(registry);
   }
 
+  @BeforeEach
+  void beforeEach() {
+    TestUser user = TestUser.JOE;
+    testClient.callLogin(new LoginRequestDTO(user.getUsername(), user.getPassword()));
+  }
+
   @Test
   void createTeam() {
     // Given
     String teamName = "Pogo Champions";
-    TestUser user = TestUser.JOE;
-    testClient.callLogin(new LoginRequestDTO(user.getUsername(), user.getPassword()));
 
     // When
     ResponseEntity<TeamDTO> created = testClient.callCreateTeam(new NewTeamDTO(teamName));
@@ -61,5 +69,47 @@ public class TeamControllerIntegrationTest {
         .extracting(ResponseEntity::getBody)
         .extracting(TeamDTO::name)
         .isEqualTo(teamName);
+  }
+
+  @Test
+  void registerMembers() {
+    // Given
+    String teamName = "Boys from the Dwarf";
+    String memberName1 = "Dave Lister";
+    String memberName2 = "Arnold Rimmer";
+    String memberName3 = "The Cat";
+    String memberName4 = "Kryten";
+
+    ResponseEntity<TeamDTO> team = testClient.callCreateTeam(new NewTeamDTO(teamName));
+    ResponseEntity<CompetitorDTO> member1 =
+        testClient.callCreateCompetitor(
+            new NewCompetitorDTO(CompetitorType.INDIVIDUAL, memberName1));
+    ResponseEntity<CompetitorDTO> member2 =
+        testClient.callCreateCompetitor(
+            new NewCompetitorDTO(CompetitorType.INDIVIDUAL, memberName2));
+    ResponseEntity<CompetitorDTO> member3 =
+        testClient.callCreateCompetitor(
+            new NewCompetitorDTO(CompetitorType.INDIVIDUAL, memberName3));
+    ResponseEntity<CompetitorDTO> member4 =
+        testClient.callCreateCompetitor(
+            new NewCompetitorDTO(CompetitorType.INDIVIDUAL, memberName4));
+    assertThat(team.getStatusCode().is2xxSuccessful()).isTrue();
+    assertThat(member1.getStatusCode().is2xxSuccessful()).isTrue();
+    assertThat(member2.getStatusCode().is2xxSuccessful()).isTrue();
+    assertThat(member3.getStatusCode().is2xxSuccessful()).isTrue();
+    assertThat(member4.getStatusCode().is2xxSuccessful()).isTrue();
+
+    // When
+    // Deliberately leave Rimmer out, he likes Morris Dancing
+    testClient.callRegisterMember(team.getBody().id(), member1.getBody().id());
+    testClient.callRegisterMember(team.getBody().id(), member3.getBody().id());
+    testClient.callRegisterMember(team.getBody().id(), member4.getBody().id());
+    ResponseEntity<TeamDTO> teamAfter = testClient.callGetTeam(team.getBody().id());
+
+    // Then
+    assertThat(teamAfter.getStatusCode().is2xxSuccessful()).isTrue();
+    assertThat(teamAfter.getBody().members())
+        .containsExactlyInAnyOrder(member1.getBody(), member3.getBody(), member4.getBody())
+        .doesNotContain(member2.getBody());
   }
 }
