88887579d9d7da2aacef4a8f51e2bd04e2a95742
Joe Sharp <ratracejoe@hotmail.com>
2025-10-01 22:24:09 +0100

    fleshing out bruno collection, cleaning up new event DTO

--- sports-day-service/adapters/service/service-rest-adapter/src/main/java/com/ratracejoe/sportsday/rest/model/NewEventDTO.java
+++ sports-day-service/adapters/service/service-rest-adapter/src/main/java/com/ratracejoe/sportsday/rest/model/NewEventDTO.java
@@ -1,13 +1,11 @@
 package com.ratracejoe.sportsday.rest.model;
 
 import com.ratracejoe.sportsday.domain.model.CompetitorType;
-import com.ratracejoe.sportsday.domain.model.EventState;
 import com.ratracejoe.sportsday.domain.model.ScoreType;
 import java.util.UUID;
 
 public record NewEventDTO(
     UUID activityId,
-    EventState state,
     CompetitorType competitorType,
     ScoreType scoreType,
     int maxParticipants) {}
